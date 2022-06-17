ddbf25e23d1bf260cb230c6d6bd1cd58507bfebe
rubik <rubixgeek795@gmail.com>
2022-06-17 08:17:00 +0800

    init

---
+++ demo2/demo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo2
+//
+//  Created by 老钱老钱 on 2022/6/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demo2/demo2/demo2App.swift
@@ -0,0 +1,17 @@
+//
+//  demo2App.swift
+//  demo2
+//
+//  Created by 老钱老钱 on 2022/6/17.
+//
+
+import SwiftUI
+
+@main
+struct demo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
